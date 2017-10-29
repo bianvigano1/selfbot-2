8f960833cadc6173357e6272c833ad0e2b3b5e25
PapaJohn <Camden179@gmail.com>
2017-10-29 10:38:24 -0400

    Fixing isdiscrim precondition

--- src/preconditions/isdiscrim.js
+++ src/preconditions/isdiscrim.js
@@ -1,7 +1,7 @@
 const patron = require('patron.js');
 
 class IsDiscrim extends patron.ArgumentPrecondition {
-  async run(command, context, argument, value) {
+  async run(command, msg, argument, args, value) {
     if (value.replace(/[^0-9]/gi, '').length !== 4 || value.length !== 4) {
       return patron.PreconditionResult.fromError(command, 'You have provided an invalid discrim');
     }
